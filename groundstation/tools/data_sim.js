4a8827be8fd8dceccc24bf5e5225db46a671a665
ajfisher <ajfisher.td@gmail.com>
2017-11-25 11:03:57 +1100

    added host and topic control to simulator closes #7

--- groundstation/tools/data_sim.js
+++ groundstation/tools/data_sim.js
@@ -1,13 +1,26 @@
+let program = require("commander");
 const mqtt = require("mqtt");
 
-const mqtt_host = "mqtt://iot.eclipse.org";
-const mqtt_topic = "rocket_surgery/data";
+program
+    .version("0.1.0")
+    .option("-f, --freq <n>", "ms between messages", parseInt)
+    .option("-h, --host [value]", "Hostname for MQTT server")
+    .option("-p, --proto [value]", "Protocol [type mqtt, mqtts, http, https]")
+    .option("-t, --topic [value]", "Topic to publish to")
+    .parse(process.argv)
 
-const speed = 2000;
+
+const mqtt_host = program.host || "iot.eclipse.org";
+const mqtt_protocol = program.proto || "mqtt";
+const mqtt_topic = program.topic || "rocket_surgery/data";
+
+const speed = program.freq || 300;
+
+console.log(mqtt_host, mqtt_protocol, mqtt_topic, speed);
 
 const start_time = Date.now();
 
-let client  = mqtt.connect(mqtt_host);
+let client  = mqtt.connect(mqtt_protocol + "://" + mqtt_host);
 
 client.on('connect', function () {
     console.log("Connected");
@@ -18,13 +31,13 @@ let intv = setInterval(() => {
         const payload = {
             id: "30aea4391908",
             t: Date.now() - start_time,
-            acc: Math.random() * 3,
-            gyr: Math.random() * 2000,
-            alt: Math.random() * 200,
+            acc: (Math.random() * 6) - 3,
+            //gyr: Math.random() * 2000,
+            alt: (Math.random() * 10) + 30,
         }
 
         client.publish(mqtt_topic, JSON.stringify(payload));
         console.log(payload);
     }
-}, 5000);
+}, speed);
 
